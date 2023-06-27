fc8398c481c875d66c4f2d7bf4b1befe973fbc85
MiguelSanLeon <>
2023-06-27 12:45:34 +0000

    Add pandas and connect it to worksheet

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
+import pandas as pd
 from google.oauth2.service_account import Credentials
+from colorama import Fore, Style, Back
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +14,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('holidays survey')
 
-pre_answers = SHEET.worksheet('predefined_answers')
+# connect Pandas to Google Sheets
+# code taken from GitHub user orlagh-sweeney
+data = SHEET.worksheet('survey_answers').get_all_values()
+headers = data.pop(0)
+df = pd.DataFrame(data, columns=headers)
+df.head()
 
-data = pre_answers.get_all_values()
+# Colorama colours for the terminal
+RED = Fore.RED  # red text for error messages
+WHITE = Fore.WHITE  # white text for instructions
+YELLOW = Fore.YELLOW  # yellow text for inputs
+BLUE = Fore.BLUE  # blue text for tables
+BACKGROUND = Back.WHITE  # white background for tables
+RESET = Style.RESET_ALL  # resets the colours
 
-print(data)
